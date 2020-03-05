f06ef418b579b1f5bb0a3b44a6c4159892b3f400
Ehren Braun <ehrenbraun16@augustana.edu>
2020-03-04 23:38:24 -0600

    Added GoogleLogin API and created login component working with App.js

---
+++ MyNotes/src/components/Login.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import '../App.css';
+import GoogleLogin from 'react-google-login';
+import App from '../App';
+
+export const Login = () => {
+      const responseGoogle = (response) =>{
+        
+      };
+    return(
+        <GoogleLogin
+        clientId="303167274627-fiqmq21s120j5ngtck0trq35b39dcu3n.apps.googleusercontent.com"
+      buttonText="Login"
+      onSuccess={responseGoogle}
+      onFailure={responseGoogle}
+      cookiePolicy={'single_host_origin'}
+      />
+    )
+}
+
+export default Login;
