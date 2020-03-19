e957692811d78ce07a81c757caf8872256fe60c4
Ehren Braun <ehrenbraun16@augustana.edu>
2020-03-18 23:05:29 -0500

    cleaned up code using react hook

--- MyNotes/src/components/GLogin.js
+++ MyNotes/src/components/GLogin.js
@@ -1,40 +1,29 @@
-import React, { Component } from 'react';
+import React from 'react';
 import firebase from './Firebase/firebase';
 
-class SignInGoogleBase extends Component {
-    constructor(props) {
-      super(props);
-      this.state = { error: null};
-    }   
-    
-    onSubmit = event => {
-  
-      event.preventDefault();
-      firebase.doSignInWithGoogle();
-      if(firebase.auth != null) {
-        var email = firebase.auth.currentUser.email;
-        var userRef = firebase.firestore.collection("users").doc(email);
-        userRef.get().then(function(doc) {
-          if (!doc.exists){
-            userRef.set({
-              name: firebase.auth.currentUser.displayName
-            });
-          }         
-        })
-      }
-    };
+// Assistance:
+// https://www.robinwieruch.de/complete-firebase-authentication-react-tutorial
 
-    render() {
-      const { error } = this.state;
-      return (
-      <div>
-        <form onSubmit={this.onSubmit}>
-          <button type="submit">Sign In</button>
-          {error && <p>{error.message}</p>}
-        </form>
-      </div>
-      );
+const GLogin = () => {
+  const signIn = event => {
+    event.preventDefault();
+    firebase.doSignInWithGoogle();
+    if(firebase.auth != null) {
+      var email = firebase.auth.currentUser.email;
+      var username = firebase.auth.currentUser.displayName;
+      var userRef = firebase.firestore.collection("users").doc(email);
+      userRef.get().then(function(doc) {
+        if(!doc.exists){
+          userRef.set({ name: username});
+        }
+      })
     }
   }
+  return(
+    <div>
+        <button onClick={signIn}>Sign In</button>
+    </div>
+  )
+}
 
-  export default SignInGoogleBase;
+  export default GLogin;
